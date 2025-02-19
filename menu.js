482d49bd2677e139173bff90103c96838d78c4f6
Jasmin Hedlund <jasminhedlund@Jasmins-MacBook-Air.local>
2025-02-19 17:01:31 +0100

    updated tml, css and javascript file

--- menu.js
+++ menu.js
@@ -1,11 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const hamburger = document.querySelector(".hamburger");
-  const navLinks = document.querySelector(".nav-links");
+  // Hamburger Menu
+  const hamburger = document.querySelector(".hamburger")
+  const navLinks = document.querySelector(".nav-links")
 
   hamburger.addEventListener("click", function () {
-    navLinks.classList.toggle("nav-active");
+    navLinks.classList.toggle("nav-active")
+    hamburger.classList.toggle("toggle")
+  })
 
-    // Animate hamburger icon to 'X'
-    hamburger.classList.toggle("toggle");
-  });
-});
+  // Elements
+  const openformButton = document.querySelector(".open-button")
+  const closeformButton = document.querySelector(".close-button")
+  const popupContainer = document.getElementById("popup-container")
+
+  // Button clicked, form pops up
+  openformButton.addEventListener("click", () => {
+    popupContainer.style.display = "block"
+  })
+
+
+  // Button clicked, form close
+  closeformButton.addEventListener("click", () => {
+    popupContainer.style.display = "none"
+  })
+
+})
+
+
