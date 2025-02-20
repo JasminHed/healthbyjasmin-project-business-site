e0fd63a1e51acf431b9726675d642d80f1d4cc28
Jasmin Hedlund <jasminhedlund@Jasmins-MacBook-Air.local>
2025-02-20 16:06:22 +0100

    updated html, css and javascript file

--- menu.js
+++ menu.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
     navLinks.classList.toggle("nav-active")
     hamburger.classList.toggle("toggle")
   })
-
+  // Click on a link + menu disappears
   const navItems = document.querySelectorAll(".nav-links li a")
   navItems.forEach((item) => {
     item.addEventListener("click", () => {
@@ -17,15 +17,16 @@ document.addEventListener("DOMContentLoaded", () => {
     })
   })
 
-  // Elements
-  const openformButton = document.querySelector(".open-button")
+  // Select Elements
   const closeformButton = document.querySelector(".close-button")
   const popupContainer = document.getElementById("popup-container")
 
-  // Open Popup
-  openformButton.addEventListener("click", () => {
-    popupContainer.classList.add("visible")
-  })
+  //Popup after 10 sec
+  setTimeout(() => {
+    if (popupContainer) {
+      popupContainer.classList.add("visible")
+    }
+  }, 10000) // = 10 seconds
 
   // Close Popup
   closeformButton.addEventListener("click", () => {
