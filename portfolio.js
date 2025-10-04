25dfe5aa9759b6eb1e5c5541eb3d788f08cff2d9
Jasmin Hedlund <jasminhedlund@gmail.com>
2025-10-04 18:51:22 +0200

    new colors on portfolio and landing page, new fonts on ladning, new images in portfolio

--- portfolio.js
+++ portfolio.js
@@ -2,7 +2,7 @@
 const projects = [
   {
       title: "Unga Allergiker",
-      image: "assets/mockup1.png",
+      image: "assets/UA.png",
       description: "Updated and developed WordPress website focusing on accessibility, performance, and SEO. As well as making the site responsive. Visual identity and images were outside project scope.",
       tech: "WordPress, CSS, JavaScript",
       link: "https://www.ungaallergiker.se/",
@@ -10,7 +10,7 @@ const projects = [
   },
   {
       title: "Kodcentrum",
-      image: "assets/mockup2.png",
+      image: "assets/kodc.png",
       description: "Updated and developed Squarespace website focusing on accessibility, SEO, and site structure as well as making the site responsive.",
       tech: "Squarespace, CSS, JavaScript",
       link: "https://kodcentrum.se/",
@@ -18,7 +18,7 @@ const projects = [
   },
   {
       title: "Zikki Website",
-      image: "assets/mockup4.png",
+      image: "assets/Zikki.png",
       description: "Built and developed business website in Squarespace using custom code. Worked with existing visual identity and images. Created content, structure, and design in collaboration with the company. Also added accessibility, responsiveness and SEO features.",
       tech: "Squarespace, CSS, JavaScript, E-commerce",
       link: "https://zikkidesign.com",
@@ -26,7 +26,7 @@ const projects = [
   },
   {
     title: "Cyberarena",
-    image: "assets/mockup3.png",
+    image: "assets/Cyber.png",
     description: "Developed and updated Squarespace website for Kodcentrum's Cyberarena. Designed color scheme, site structure, and all content including images and text. Built with custom code, focusing on accessibility, responsiveness and SEO optimization.",
     tech: "Squarespace, CSS, JavaScript",
     link: "https://zikkidesign.com",
