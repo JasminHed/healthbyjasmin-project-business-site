9073a088f9ee3742b22e9d3062bef36d24df4c91
Jasmin Hedlund <jasminhedlund@gmail.com>
2025-10-15 06:20:26 +0200

    updated portolio with projects, updated with prices

--- portfolio.js
+++ portfolio.js
@@ -32,9 +32,35 @@ const projects = [
     image: "assets/Cyber.png",
     description: "Developed and updated Squarespace website for Kodcentrum's Cyberarena. Designed color scheme, site structure, and all content including images and text. Built with custom code, focusing on accessibility, responsiveness and SEO optimization.",
     tech: "Squarespace, CSS, JavaScript",
-    link: "https://zikkidesign.com",
+    link: "https://cyberarena.se",
      linkText: "Visit Cyberarena",
      note: "*This project has been handed over to the client. Later updates and design changes may differ from the original delivery."
+},
+{
+  title: "Intention Hub",
+  image: "assets/intentionhub.png",
+  description: "A fully custom-coded website built from scratch without using templates or CMS platforms. A intention hub that helps users set intentions and goals with an AI bot for guidance and a supportive community.",
+  tech: "React, styled components, MongoDB, Node.js",
+  link: "https://intentionhub.netlify.app/",
+   linkText: "Visit Intention Hub",
+   note: "*This project is still being developed and updated."
+},
+{
+  title: "Movie App",
+  image: "assets/movie.png",
+  description: "A fully custom-coded website built from scratch without using templates or CMS platforms. A movie discovery website that displays popular and new films across all categories with real-time updates. ",
+  tech: "React, styled components, API",
+  link: "https://movie-projectapp.netlify.app/",
+   linkText: "Visit The movie App",
+},
+{
+  title: "HeidikaYoga",
+  image: "assets/heidi.png",
+  description: "Built and developed business website in Squarespace using custom code. Worked with existing visual identity and images. Created content, structure, and design in collaboration with the company. Also added accessibility, responsiveness and SEO features.",
+  tech: "Squarespace, CSS, JavaScript, E-commerce",
+  link: "https://heidikayoga.com",
+   linkText: "Visit Heidikayoga",
+  note: "*This project has been handed over to the client. Later updates and design changes may differ from the original delivery."
 }
 ];
 
